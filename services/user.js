3e59c53056b32c97e49b8d43547d973487b8c3c5
BuildTools <unconfigured@null.spigotmc.org>
2016-08-31 01:43:08 +0800

    fix null result from mysql

--- services/user.js
+++ services/user.js
@@ -38,7 +38,7 @@ User.getUserId = function getUserId(name) {
 
         console.log(result);
 
-        if (result.length != 0) {
+        if (result.length !== 0) {
             return result[0].id;
         }
         return false;
